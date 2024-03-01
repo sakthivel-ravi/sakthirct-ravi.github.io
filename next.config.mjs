41404289e927254b6c0ba5cb80e5bc26009ce3bb
User-019 <sakthivel-ravi>
2024-03-02 00:02:31 +0530

    out folder added

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  output: "export",
+  // output: "export",
+  trailingSlash: true,
 };
 
 export default nextConfig;
